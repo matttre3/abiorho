082fd88bfbef6ccd3bd2c90a5b5a62729c278026
Matteo Romano <3romano.matteo3@gmail.com>
2024-12-07 22:10:30 +0100

    fix images

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,14 @@ import Gallery from "@/components/Gallery";
 
 export default function Home() {
   const images = [
-    "/images/1.JPG",
-    "/images/2.JPG",
+    "/images/1.jpg",
+    "/images/2.jpg",
     "/images/3.jpg",
-    "/images/4.JPG",
+    "/images/4.jpg",
     "/images/8.jpg",
-    "/images/5.JPG",
-    "/images/6.JPG",
-    "/images/7.JPG",
+    "/images/5.jpg",
+    "/images/6.jpg",
+    "/images/7.jpg",
     "/images/9.jpg",
   ];
 
